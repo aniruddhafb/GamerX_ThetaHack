cc545df7e758a2faaaf2e58ec04a1d8e99d6ea76
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-04-14 09:48:06 +0530

    added wallet connect and theta video api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,100 +1,131 @@
-import React from 'react'
-import gamerXlogo from "../../public/logo.png"
-import Image from 'next/image'
-import Link from 'next/link'
+import React from "react";
+import gamerXlogo from "../../public/logo.png";
+import Image from "next/image";
+import Link from "next/link";
 
-const Navbar = () => {
-    return (
-        <header>
-            <div id="sticky-header" className="tg-header__area transparent-header" style={{ backgroundColor: "#182029" }}>
-                <div className="container custom-container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="mobile-nav-toggler"><i className="fas fa-bars"></i></div>
-                            <div className="tgmenu__wrap">
-                                <nav className="tgmenu__nav">
-                                    <div className="logo">
-                                        <Link href="/"><Image
-                                            src={gamerXlogo}
-                                            alt="Logo" /></Link>
-                                    </div>
-                                    <div className="tgmenu__navbar-wrap tgmenu__main-menu d-none d-xl-flex">
-                                        <ul className="navigation">
-                                            <li><Link href="/" style={{ textDecoration: "none" }}>Home</Link></li>
-                                            <li><Link href="/about" style={{ textDecoration: "none" }}>About us</Link></li>
-                                            <li className="menu-item-has-children"><Link
-                                                href="#" style={{ textDecoration: "none" }}>Other Pages</Link>
-                                                <ul className="sub-menu">
-                                                    <li><Link
-                                                        href="/topGamers">Gamers</Link>
-                                                    </li>
-                                                    <li><Link href="/topNFTs">All NFTs</Link></li>
-                                                </ul>
-                                                <div className="dropdown-btn"><span className="plus-line"></span></div>
-                                            </li>
-                                            <li className="menu-item-has-children"><Link
-                                                href="#" style={{ textDecoration: "none" }}>Create</Link>
-                                                <ul className="sub-menu">
-                                                    <li><Link href="/uploadVideo">Upload Video</Link>
-                                                    </li>
-                                                </ul>
-                                                <div className="dropdown-btn"><span className="plus-line"></span></div>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div className="tgmenu__action d-none d-md-block">
-                                        <ul className="list-wrap">
-                                            <li className="header-btn">
-                                                <Link href="contact.html"
-                                                    className="tg-border-btn">
-                                                    Connect Wallet
-                                                </Link>
-                                            </li>
+const Navbar = ({ connect_wallet }) => {
+  return (
+    <header>
+      <div
+        id="sticky-header"
+        className="tg-header__area transparent-header"
+        style={{ backgroundColor: "#182029" }}
+      >
+        <div className="container custom-container">
+          <div className="row">
+            <div className="col-12">
+              <div className="mobile-nav-toggler">
+                <i className="fas fa-bars"></i>
+              </div>
+              <div className="tgmenu__wrap">
+                <nav className="tgmenu__nav">
+                  <div className="logo">
+                    <Link href="/">
+                      <Image src={gamerXlogo} alt="Logo" />
+                    </Link>
+                  </div>
+                  <div className="tgmenu__navbar-wrap tgmenu__main-menu d-none d-xl-flex">
+                    <ul className="navigation">
+                      <li>
+                        <Link href="/" style={{ textDecoration: "none" }}>
+                          Home
+                        </Link>
+                      </li>
+                      <li>
+                        <Link href="/about" style={{ textDecoration: "none" }}>
+                          About us
+                        </Link>
+                      </li>
+                      <li className="menu-item-has-children">
+                        <Link href="#" style={{ textDecoration: "none" }}>
+                          Other Pages
+                        </Link>
+                        <ul className="sub-menu">
+                          <li>
+                            <Link href="/topGamers">Gamers</Link>
+                          </li>
+                          <li>
+                            <Link href="/topNFTs">All NFTs</Link>
+                          </li>
+                        </ul>
+                        <div className="dropdown-btn">
+                          <span className="plus-line"></span>
+                        </div>
+                      </li>
+                      <li className="menu-item-has-children">
+                        <Link href="#" style={{ textDecoration: "none" }}>
+                          Create
+                        </Link>
+                        <ul className="sub-menu">
+                          <li>
+                            <Link href="/uploadVideo">Upload Video</Link>
+                          </li>
+                        </ul>
+                        <div className="dropdown-btn">
+                          <span className="plus-line"></span>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="tgmenu__action d-none d-md-block">
+                    <ul className="list-wrap">
+                      <li className="header-btn">
+                        <div onClick={connect_wallet} className="tg-border-btn">
+                          Connect Wallet
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
 
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-
-                            {/* <!-- Mobile Menu  --> */}
-                            <div className="tgmobile__menu">
-                                <nav className="tgmobile__menu-box">
-                                    <div className="close-btn"><i className="flaticon-swords-in-cross-arrangement"></i></div>
-                                    <div className="nav-logo">
-                                        <Link href="index.html">
-                                            <img src="logo.png"
-                                                alt="Logo" /></Link>
-                                    </div>
-                                    {/* <div className="tgmobile__search">
+              {/* <!-- Mobile Menu  --> */}
+              <div className="tgmobile__menu">
+                <nav className="tgmobile__menu-box">
+                  <div className="close-btn">
+                    <i className="flaticon-swords-in-cross-arrangement"></i>
+                  </div>
+                  <div className="nav-logo">
+                    <Link href="index.html">
+                      <img src="logo.png" alt="Logo" />
+                    </Link>
+                  </div>
+                  {/* <div className="tgmobile__search">
                                         <form action="#">
                                             <input type="text" placeholder="Search here..." />
                                             <button><i className="flaticon-loupe"></i></button>
                                         </form>
                                     </div> */}
-                                    <div className="tgmobile__menu-outer">
-                                        {/* popup menu */}
+                  <div className="tgmobile__menu-outer">
+                    {/* popup menu */}
 
-                                        <ul className="navigation">
-                                            <li><Link href="#">Home</Link></li>
-                                            <li><Link href="#">ABOUT US</Link></li>
-                                        </ul>
-                                    </div>
-                                    <div className="social-links">
-                                        <ul className="list-wrap">
-                                            <li><Link href="#"><i
-                                                className="fab fa-twitter"></i></Link></li>
-                                        </ul>
-                                    </div>
-                                </nav>
-                            </div>
-                            <div className="tgmobile__menu-backdrop"></div>
-                        </div>
-                    </div>
-                </div>
+                    <ul className="navigation">
+                      <li>
+                        <Link href="#">Home</Link>
+                      </li>
+                      <li>
+                        <Link href="#">ABOUT US</Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="social-links">
+                    <ul className="list-wrap">
+                      <li>
+                        <Link href="#">
+                          <i className="fab fa-twitter"></i>
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+              <div className="tgmobile__menu-backdrop"></div>
             </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-        </header>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,138 @@
-import Footer from '@/components/Footer'
-import Navbar from '@/components/Navbar'
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { useState, useEffect } from "react";
+// styles
+import "@/styles/globals.css";
+import "@/styles/tailwind.css";
+import "@/styles/plugins.css";
+import "@/styles/custom.css";
 
-// styles 
-import '@/styles/globals.css'
-import '@/styles/tailwind.css'
-import '@/styles/plugins.css'
-import '@/styles/custom.css'
+//polybasE
+import { Polybase } from "@polybase/client";
+import { ethers } from "ethers";
 
 export default function App({ Component, pageProps }) {
+  const [signer, set_signer] = useState();
+  const db = new Polybase({
+    defaultNamespace: process.env.NEXT_PUBLIC_NAMESPACE,
+  });
+
+  const connect_wallet = async () => {
+    let provider;
+    let signer;
+    try {
+      if (window.ethereum == null) {
+        console.log("MetaMask not installed; using read-only defaults");
+        provider = ethers.getDefaultProvider();
+      } else {
+        provider = new ethers.BrowserProvider(window.ethereum);
+        signer = await provider.getSigner();
+        const signer_address = await signer.getAddress();
+        console.log({ signer_address });
+        const check_user = await db
+          .collection("User")
+          .where("id", "==", signer_address)
+          .get();
+        if (!check_user) {
+          db.collection("User").create([signer_address]);
+        }
+      }
+    } catch (error) {
+      console.log({ connect_wallet: error.message });
+    }
+  };
+
+  const upload_video = async (e, file) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios({
+        url: "https://api.thetavideoapi.com/upload",
+        method: "POST",
+        headers: {
+          "x-tva-sa-id": "srvacc_sqc1y5hhxz838uune4zh519a3",
+          "x-tva-sa-secret": "hcj34emsvgy9xmtuau31n5ab53624px3",
+        },
+      });
+      console.log(res.data.body.uploads[0].id);
+      console.log(res.data.body.uploads[0]);
+
+      const res2 = await axios({
+        url: res.data.body.uploads[0].presigned_url,
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/octet-stream",
+        },
+        data: file,
+      });
+
+      console.log(res2);
+
+      //GET VIDEO ID
+      const res3 = await axios({
+        method: "POST",
+        url: "https://api.thetavideoapi.com/video",
+        headers: {
+          "x-tva-sa-id": "srvacc_sqc1y5hhxz838uune4zh519a3",
+          "x-tva-sa-secret": "hcj34emsvgy9xmtuau31n5ab53624px3",
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          source_upload_id: res.data.body.uploads[0].id,
+          playback_policy: "public",
+        }),
+      });
+
+      console.log(res3.data.body.videos[0].id);
+
+      //GET VIDEO STATUS
+      const res4 = await axios({
+        url: `https://api.thetavideoapi.com/video/${res3.data.body.videos[0].id}`,
+        method: "GET",
+        headers: {
+          "x-tva-sa-id": "srvacc_sqc1y5hhxz838uune4zh519a3",
+          "x-tva-sa-secret": "hcj34emsvgy9xmtuau31n5ab53624px3",
+        },
+      });
+
+      console.log(res4.data);
+
+      const res5 = await axios({
+        url: `https://api.thetavideoapi.com/video/srvacc_sqc1y5hhxz838uune4zh519a3/list?page=1&number=100`,
+        method: "GET",
+        headers: {
+          "x-tva-sa-id": "srvacc_sqc1y5hhxz838uune4zh519a3",
+          "x-tva-sa-secret": "hcj34emsvgy9xmtuau31n5ab53624px3",
+        },
+      });
+
+      console.log(res5.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+    // videojs("my-player", {
+    //   techOrder: ["theta_hlsjs", "html5"],
+    //   sources: [
+    //     {
+    //       src: "https://media.thetavideoapi.com/video_kegqg8x8pz63rxqv650jzh8az6/master.m3u8",
+    //       type: "application/vnd.apple.mpegurl",
+    //       label: "1080p",
+    //     },
+    //   ],
+    //   theta_hlsjs: {
+    //     videoId: "video_kegqg8x8pz63rxqv650jzh8az6",
+    //   },
+    // });
+  };
+
+  useEffect(() => {
+    connect_wallet();
+  }, []);
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
+      <Navbar connect_wallet={connect_wallet} />
+      <Component {...pageProps} upload_video={upload_video} />
       <Footer />
     </>
-  )
+  );
 }
