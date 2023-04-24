32e42a70eac59db1078d611fd71e6952b8faa510
shravanandoria <36509067+shravanandoria@users.noreply.github.com>
2023-04-24 15:55:45 +0530

    deployed default nft collection contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    theta: {
+      chainId: 365,
+      accounts: [process.env.NEXT_PUBLIC_PRIVATE_KEY],
+      url: "https://eth-rpc-api-testnet.thetatoken.org/rpc",
+    },
+  },
 };
--- scripts/collection.js
+++ scripts/collection.js
@@ -7,21 +7,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  const gamerx_collection = await hre.ethers.getContractFactory(
+    "NFTCollection"
   );
+  const GamerX_contreact = await gamerx_collection.deploy(
+    "GamerX NFT Collection",
+    "GamerX"
+  );
+
+  await GamerX_contreact.deployed();
+
+  console.log(` NFT Collection address: ${GamerX_contreact.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
