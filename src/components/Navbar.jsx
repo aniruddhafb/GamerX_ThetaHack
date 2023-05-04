73d7dd6b429f70d030db3f53bfeb4cde6bef2f26
Aniruddha <aniruddhavikharankarfb@gmail.com>
2023-05-04 21:18:16 +0530

    push notifications and minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import gamerXlogo from "../../public/logo.png";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,8 +8,14 @@ import { useRouter } from "next/router";
 import * as PushAPI from "@pushprotocol/restapi";
 
 
-const Navbar = ({ connect_wallet, signerAddress, signer_bal, chainID, setChainID }) => {
+const Navbar = ({ connect_wallet, signer, signerAddress, signer_bal, chainID, setChainID, GAMERX_CHANNEL_ADDRESS }) => {
+
   const router = useRouter();
+  const [notificationData, setNotificationData] = useState();
+  const [nullNotification, setNullNotification] = useState(true);
+  const [optedIn, setOptedIn] = useState(false);
+  const [showNotifications, SetShowNotifications] = useState(false);
+
   const [showProfileDrop, setShowProfileDrop] = useState(false);
   const [showMobileNav, setShowMobileNav] = useState(false);
 
@@ -22,6 +28,7 @@ const Navbar = ({ connect_wallet, signerAddress, signer_bal, chainID, setChainID
     }
   }
 
+  // switch chain 
   const switchThetaChain = async () => {
     try {
       await window.ethereum.request({
@@ -55,6 +62,46 @@ const Navbar = ({ connect_wallet, signerAddress, signer_bal, chainID, setChainID
       }
     }
   };
+
+  // optin for push noti
+  const optInToChannel = async () => {
+    await PushAPI.channels.subscribe({
+      env: "staging",
+      signer: signer,
+      channelAddress: `eip155:${chainID}:${GAMERX_CHANNEL_ADDRESS}`,
+      userAddress: `eip155:${chainID}:${signerAddress}`,
+      onSuccess: () => {
+        setOptedIn(true);
+      },
+      onError: (err) => {
+        console.error("opt-in error", err);
+      },
+    });
+  };
+
+  // push get noti 
+  const getNotifications = () => {
+    PushAPI.user
+      .getFeeds({
+        user: `eip155:${chainID}:${signerAddress}`,
+        env: "staging",
+        page: 1,
+        limit: 10,
+      })
+      .then((feeds) => {
+        setNotificationData(feeds);
+        if (feeds[0]?.app != "RarX Marketplace") {
+          setNullNotification(false);
+        }
+      })
+      .catch((err) => {
+        console.error("failed to get user notifications: ", err);
+      });
+  };
+
+  useEffect(() => {
+    getNotifications();
+  })
 
   return (
     <header className={`${showMobileNav && "mobile-menu-visible"}`}>
@@ -165,25 +212,151 @@ const Navbar = ({ connect_wallet, signerAddress, signer_bal, chainID, setChainID
                   {signerAddress && signerAddress ?
                     <>
                       {/* Notifications  */}
-                      <div className="notifiCustom relative mr-2 mb-[4px]">
-                        <button
-                          className="hidden text-gray-400 mt-[5px] transition-colors duration-300 transform lg:block hover:text-gray-600 text-base text-jacarta-700"
-                        >
-                          <svg
-                            className="w-7 h-7"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
+                      <div className="relative">
+                        <div className="notifiCustom relative mr-2 mb-[4px]">
+                          <button
+                            onClick={() => (
+                              getNotifications(),
+                              SetShowNotifications(!showNotifications)
+                            )}
+                            className="hidden text-gray-400 mt-[5px] transition-colors duration-300 transform lg:block hover:text-gray-600 text-base text-jacarta-700"
                           >
-                            <path
-                              d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                              stroke="currentColor"
-                              strokeWidth="2"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            />
-                          </svg>
-                        </button>
+                            <svg
+                              className="w-7 h-7"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+
+                        {/* {showNotifications && (
+                          <div className="absolute right-[-50px] z-20 w-64 mt-6 overflow-hidden origin-top-right bg-white rounded-md shadow-xl sm:w-80 dark:bg-gray-800">
+                            {notificationData && notificationData?.map((e, i) => {
+                              return (
+                                i < 6 &&
+                                e.app === "RarX Marketplace" && (
+                                  <div key={e.sid}>
+                                    <a
+                                      href={e.cta}
+                                      rel="noreferrer"
+                                      target="_blank"
+                                      className="flex items-center px-4 py-3 -mx-2 transition-colors duration-300 transform border-b border-gray-100 hover:bg-gray-100"
+                                    >
+                                      <Image
+                                        className="flex-shrink-0 object-cover w-8 h-8 mx-1 rounded-full"
+                                        src={e.icon}
+                                        alt="avatar"
+                                        height={100}
+                                        width={100}
+                                      />
+                                      <p className="mx-2 text-sm text-black flex flex-col">
+                                        <a
+                                          className="font-bold text-[12px]"
+                                          href={e.cta}
+                                          target="_blank"
+                                          rel="noreferrer"
+                                        >
+                                          {e.notification.title}
+                                        </a>
+                                        <span className="font-mono text-[10px]">
+                                          {e.notification.body}
+                                        </span>
+                                      </p>
+                                    </a>
+                                  </div>
+                                )
+                              );
+                            })}
+                            {notificationData && notificationData[0]?.app != "RarX Marketplace" && (
+                              <div className="h-[135px]">
+                                <a
+                                  href="#"
+                                  rel="noreferrer"
+                                  className="flex items-center px-4 py-3 transition-colors duration-300 transform border-gray-100"
+                                >
+                                  <p className="mx-2 text-sm text-black flex flex-col">
+                                    <a
+                                      className="font-bold text-[12px]"
+                                      href="#"
+                                      rel="noreferrer"
+                                    >
+                                      No Notifications yet! click below button to
+                                      susbcribe the rarx marketplace notification
+                                      channel, ignore if already subscribed
+                                    </a>
+                                    {optedIn ? (
+                                      <button
+                                        type="button"
+                                        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-4 mt-4"
+                                        style={{ backgroundColor: "#6D3EEE" }}
+                                      >
+                                        Subscribed 🎉
+                                      </button>
+                                    ) : (
+                                      <button
+                                        onClick={() => optInToChannel()}
+                                        type="button"
+                                        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-4 mt-4"
+                                        style={{ backgroundColor: "#6D3EEE" }}
+                                      >
+                                        Subscribe
+                                      </button>
+                                    )}
+                                  </p>
+                                </a>
+                              </div>
+                            )}
+                            {notificationData && notificationData.length === 0 &&
+                              nullNotification == true && (
+                                <div className="h-[135px]">
+                                  <a
+                                    href="#"
+                                    rel="noreferrer"
+                                    className="flex items-center px-4 py-3 transition-colors duration-300 transform border-gray-100"
+                                  >
+                                    <p className="mx-2 text-sm text-black flex flex-col">
+                                      <a
+                                        className="font-bold text-[12px]"
+                                        href="#"
+                                        rel="noreferrer"
+                                      >
+                                        No Notifications yet! click below button to
+                                        susbcribe the rarx marketplace notification
+                                        channel, ignore if already subscribed
+                                      </a>
+                                      {optedIn ? (
+                                        <button
+                                          type="button"
+                                          className="text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-4 mt-4"
+                                          style={{ backgroundColor: "#6D3EEE" }}
+                                        >
+                                          Subscribed 🎉
+                                        </button>
+                                      ) : (
+                                        <button
+                                          onClick={() => optInToChannel()}
+                                          type="button"
+                                          className="text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-4 mt-4"
+                                          style={{ backgroundColor: "#6D3EEE" }}
+                                        >
+                                          Subscribe
+                                        </button>
+                                      )}
+                                    </p>
+                                  </a>
+                                </div>
+                              )}
+                          </div>
+                        )} */}
                       </div>
 
                       {/* // profile dropdown */}
